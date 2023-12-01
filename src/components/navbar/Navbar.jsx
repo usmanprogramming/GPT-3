df8d0a34672d3ff5589ae38e5ac543624a58d334
usmanprogramming <siddiquishoaib515@gmail.com>
2023-12-01 17:12:47 +0500

    minor classname typo fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
           )}
           {toggleMenu && (
             <div className="gpt3__navbar-menu_container scale-up-center">
-              <div className="gpt3__navbar-menu_containe-links">
+              <div className="gpt3__navbar-menu_container-links">
                 <Menu />
                 <div className="gpt3__navbar-menu_container-links-sign">
                   <p>Sign in</p>
